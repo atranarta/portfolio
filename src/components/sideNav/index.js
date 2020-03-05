305abb9b2e778f07f239fa51c061caaab48e1152
atranarta <radyuk.tatyana@gmail.com>
2020-03-05 19:31:39 +0100

    Added basic styles and nav

--- src/components/sideNav/index.js
+++ src/components/sideNav/index.js
@@ -8,6 +8,15 @@ const sideNav = () => (
         <img src={Avatar} className="avatar" alt="Tetiana Rabiievska" width="170px"></img>
       </div>
     </div>
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/">About</a></li>
+        <li><a href="/">Resume</a></li>
+        <li><a href="/">Portfolios</a></li>
+        <li><a href="/">Contact</a></li>
+      </ul>
+    </nav>
   </aside>
 );
 
