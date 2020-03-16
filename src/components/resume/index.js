49a6ddce0cd84b5366fd2e04a8e004b5fe4ea4f1
atranarta <radyuk.tatyana@gmail.com>
2020-03-16 17:31:19 +0100

    Removed percentage of my skills

--- src/components/resume/index.js
+++ src/components/resume/index.js
@@ -10,47 +10,38 @@ const Resume = (props) => (
     <div className="skills-box">
       <Skill
         title="HTML5"
-        progress="95%"
         width="95%"
       />
       <Skill
         title="ES6"
-        progress="75%"
         width="75%"
       />
       <Skill
         title="CSS3"
-        progress="90%"
         width="90%"
       />
       <Skill
         title="SCSS/SASS"
-        progress="80%"
         width="80%"
       />
       <Skill
         title="Javascript"
-        progress="60%"
         width="60%"
       />
       <Skill
         title="React"
-        progress="40%"
         width="40%"
       />
       <Skill
         title="NodeJS"
-        progress="30%"
         width="30%"
       />
       <Skill
         title="Photoshop"
-        progress="40%"
         width="40%"
       />
       <Skill
         title="AdobeXD"
-        progress="40%"
         width="40%"
       />
     </div>
--- src/components/resume/skill.js
+++ src/components/resume/skill.js
@@ -4,7 +4,6 @@ const Skill = props => (
   <div className="progress-wrap">
     <h6 className="progress-title">{props.title}</h6>
     <div className="progress">
-      <div className="progress-percentage">{props.progress}</div>
       <div className="progress-container">
         <span className="progress-active" style={{ width: props.width }}></span>
       </div>
