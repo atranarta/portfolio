f28d1a5ab6f13b1092974cf665cc3927d3084df5
atranarta <radyuk.tatyana@gmail.com>
2020-03-16 12:03:55 +0100

    Styled portfolios gallery

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -12,7 +12,7 @@ const About = () => (
     <ul>
       <li><b>Full Name</b><span>Tetiana Rabiievska</span></li>
       <li><b>Age</b><span>30</span></li>
-      <li><b>Languages</b><span>Ukrainian (Native), Russian (Fluent), English (Upper Intermediate), German (Upper Intermediate), Polish (Elementary)</span></li>
+      <li><b>Languages</b><span>English (Professional working proficiency), German (Upper Intermediate), Ukrainian (Native), Russian (Native), Polish (Elementary)</span></li>
       <li><b>Location</b><span>Berlin, Germany</span></li>
     </ul>
     <a className="btn" href="/">Download CV</a>
