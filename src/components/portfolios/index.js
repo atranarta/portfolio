16706604e3a2cb1baf10189eb7d95e5490e22604
atranarta <radyuk.tatyana@gmail.com>
2020-03-25 15:17:17 +0100

    Replaced repeating pieces of code with an array of objects and the map function in portfolio component

--- src/components/portfolios/index.js
+++ src/components/portfolios/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import Portfolio from './portfolio'
-import Lotto_game from './../../img/portfolios/lotto-game.png'
-import Calculator from './../../img/portfolios/calculator.png'
-import ToDo_List from './../../img/portfolios/todo-list.png'
 
 const Portfolios = (props) => (
   <div id="portfolios" className="component">
@@ -11,24 +8,7 @@ const Portfolios = (props) => (
       <span>Portfolios</span>
     </div>
     <div className="portfolios-wrap">
-      <Portfolio
-        img={Lotto_game}
-        imageTitle="Lotto Game"
-        title="Lotto Game"
-        text="A Lotto-game created using HTML, CSS and Javascript (by me and two more persons)"
-      />
-      <Portfolio
-        img={Calculator}
-        imageTitle="Calculator"
-        title="Calculator"
-        text="A calculator created using React and SCSS"
-      />
-      <Portfolio
-        img={ToDo_List}
-        imageTitle="ToDo List"
-        title="ToDo List"
-        text="A calculator created using React and SCSSA ToDo List created using React, Javascript and SCSS"
-      />
+      <Portfolio />
     </div>
   </div>
 );
