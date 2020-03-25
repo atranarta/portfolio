f451203064de939962e8bbbe5f12d9730ecab348
atranarta <radyuk.tatyana@gmail.com>
2020-03-25 15:02:46 +0100

    Replaced repeating pieces of code with an array of objects and .map()

--- src/components/resume/index.js
+++ src/components/resume/index.js
@@ -8,42 +8,7 @@ const Resume = (props) => (
       <span>My skills</span>
     </div>
     <div className="skills-box">
-      <Skill
-        title="HTML5"
-        width="95%"
-      />
-      <Skill
-        title="ES6"
-        width="75%"
-      />
-      <Skill
-        title="CSS3"
-        width="90%"
-      />
-      <Skill
-        title="SCSS/SASS"
-        width="80%"
-      />
-      <Skill
-        title="Javascript"
-        width="60%"
-      />
-      <Skill
-        title="React"
-        width="40%"
-      />
-      <Skill
-        title="NodeJS"
-        width="30%"
-      />
-      <Skill
-        title="Photoshop"
-        width="40%"
-      />
-      <Skill
-        title="AdobeXD"
-        width="40%"
-      />
+      <Skill />
     </div>
   </div>
 );
--- src/components/resume/skill.js
+++ src/components/resume/skill.js
@@ -1,14 +1,55 @@
 import React from 'react';
 
-const Skill = props => (
-  <div className="progress-wrap">
-    <h6 className="progress-title">{props.title}</h6>
-    <div className="progress">
-      <div className="progress-container">
-        <span className="progress-active" style={{ width: props.width }}></span>
+const skills = [
+  {
+    title: "HTML5",
+    width: "95%"
+  },
+  {
+    title: "ES6",
+    width: "75%"
+  },
+  {
+    title: "CSS3",
+    width: "90%"
+  },
+  {
+    title: "SCSS/SASS",
+    width: "80%"
+  },
+  {
+    title: "Javascript",
+    width: "60%"
+  },
+  {
+    title: "React",
+    width: "40%"
+  },
+  {
+    title: "NodeJS",
+    width: "30%"
+  },
+  {
+    title: "Photoshop",
+    width: "40%"
+  },
+  {
+    title: "AdobeXD",
+    width: "40%"
+  }
+];
+
+const Skill = () => (
+  skills.map(skill => (
+    <div className="progress-wrap">
+      <h6 className="progress-title">{skill.title}</h6>
+      <div className="progress">
+        <div className="progress-container">
+          <span className="progress-active" style={{ width: skill.width }}></span>
+        </div>
       </div>
     </div>
-  </div>
+  ))
 );
 
 export default Skill;
