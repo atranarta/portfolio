88ad1bf3be11aa44edec65e3902b1d9812cc66f1
atranarta <radyuk.tatyana@gmail.com>
2020-04-08 14:33:27 +0200

    Moved svg-icons in to separate files

--- src/components/portfolios/portfolio.js
+++ src/components/portfolios/portfolio.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Lotto_game from './../../img/portfolios/lotto-game.png';
 import Calculator from './../../img/portfolios/calculator.png';
 import ToDo_List from './../../img/portfolios/todo-list.png';
+import LoupeIcon from './loupeIcon';
+import LinkIcon from './linkIcon';
 
 const portfolios = [
   {
@@ -17,7 +19,7 @@ const portfolios = [
   {
     img: ToDo_List,
     title: "ToDo List",
-    text: "A calculator created using React and SCSS ToDo List created using React, Javascript and SCSS"
+    text: "A ToDo List created using React, Javascript and SCSS"
   },
 ]
 
@@ -30,11 +32,13 @@ const Portfolio = () => {
         <ul>
           <li onClick={() => setShowPopup(!showPopup)}>
             <button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line><line x1="11" y1="8" x2="11" y2="14"></line><line x1="8" y1="11" x2="14" y2="11"></line></svg>
+              <LoupeIcon />
             </button>
           </li>
           <li>
-            <a rel="noopener noreferrer" target="_blank" href="/"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg></a>
+            <a rel="noopener noreferrer" target="_blank" href="/">
+              <LinkIcon />
+            </a>
           </li>
         </ul>
       </div>
