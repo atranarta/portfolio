9a4e75d267113a05796bf72a2ef7e9f5a879df02
atranarta <radyuk.tatyana@gmail.com>
2020-04-17 16:55:13 +0200

    Fixes: values of inputs are visible, removed table tag to avoid reloading the page by clicking on the button

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -7,10 +7,10 @@ const Contact = () => {
   const [inputEmail, setInputEmail] = useState("");
   const [inputSubject, setInputSubject] = useState("");
   const [getUserText, setUserText] = useState("");
-  const [message, toggleMessageFunc] = useState(false);
+  const [messageBool, setMessageBool] = useState(false);
 
   const showMessage = () => {
-    toggleMessageFunc(!message);
+    setMessageBool(!messageBool);
   };
 
   return (
@@ -19,35 +19,36 @@ const Contact = () => {
         text="Contact me"
       />
       <h4>Get In Touch</h4>
-      <form>
-        {message && (
-          <div className="userMessage">{`Thank you ${inputName} from ${inputSubject} for contacting me! I'll get back to you on your email ${inputEmail} and your message: ${getUserText}`}</div>
-        )}
-        <Field id="contact-form-name"
-          label="Enter your name"
-          onChange={event => setInputName(event.target.value)} />
-        <Field id="contact-form-email"
-          label="Enter your email"
-          onChange={event => setInputEmail(event.target.value)} />
-        <Field id="contact-form-subject"
-          label="Enter your subject"
-          onChange={event => setInputSubject(event.target.value)} />
+      {messageBool && (
+        <div className="userMessage">{`Thank you ${inputName} from ${inputSubject} for contacting me! I'll get back to you on your email ${inputEmail} and your message: ${getUserText}`}</div>
+      )}
+      <Field 
+        id="contact-form-name"
+        label="Enter your name"
+        value={inputName}
+        onChange={event => setInputName(event.target.value)} />
+      <Field id="contact-form-email"
+        label="Enter your email"
+        value={inputEmail}
+        onChange={event => setInputEmail(event.target.value)} />
+      <Field id="contact-form-subject"
+        label="Enter your subject"
+        value={inputSubject}
+        onChange={event => setInputSubject(event.target.value)} />
 
-        <div className="form-field">
-          <label for="contact-form-message">Enter your message</label>
-          <textarea type="text"
-            name="name"
-            id="contact-form-message"
-            cols="30"
-            rows="6"
-            onChange={event => setUserText(event.target.value)}>
-          </textarea>
-        </div>
-        <div className="form-field">
-          <button onClick={showMessage} className="btn" target="_blank" rel="noopener noreferrer">Send Mail</button>
-        </div>
-
-      </form>
+      <div className="form-field">
+        <label for="contact-form-message">Enter your message</label>
+        <textarea type="text"
+          name="name"
+          id="contact-form-message"
+          cols="30"
+          rows="6"
+          onChange={event => setUserText(event.target.value)}>
+        </textarea>
+      </div>
+      <div className="form-field">
+        <button onClick={showMessage} className="btn" target="_blank" rel="noopener noreferrer">Send Mail</button>
+      </div>
     </div>
   )
 };
